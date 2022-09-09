117d010bd90a01200051dc7215da52063a62e85d
eduardogom23 <pinedaje@uninorte.edu.co>
2022-09-09 16:02:29 -0500

    MAYUSCULAS

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -2,7 +2,7 @@ public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camellos", "Mamifero"); 
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
+        miVertebrados[1] = new Vertebrado("VACAS", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
         return miVertebrados;
